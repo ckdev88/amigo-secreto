269f278daae063a9ab8b1752dfee50c8569ddc92
ckdev88 <ckdev88@outlook.com>
2023-08-29 16:00:11 -0300

    add & connect to supabase via API

---
+++ types.ts
@@ -0,0 +1,75 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      groups: {
+        Row: {
+          created_at: string | null
+          created_by: string | null
+          id: string
+          name: string
+        }
+        Insert: {
+          created_at?: string | null
+          created_by?: string | null
+          id?: string
+          name: string
+        }
+        Update: {
+          created_at?: string | null
+          created_by?: string | null
+          id?: string
+          name?: string
+        }
+        Relationships: []
+      }
+      members: {
+        Row: {
+          group_id: string | null
+          id: string
+          name: string
+          selected_by: string | null
+        }
+        Insert: {
+          group_id?: string | null
+          id?: string
+          name: string
+          selected_by?: string | null
+        }
+        Update: {
+          group_id?: string | null
+          id?: string
+          name?: string
+          selected_by?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "members_group_id_fkey"
+            columns: ["group_id"]
+            referencedRelation: "groups"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
