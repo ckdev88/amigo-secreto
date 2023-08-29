17555de40fa32edf7139c7e5ba64618835737a99
ckdev88 <ckdev88@outlook.com>
2023-08-29 14:26:41 -0300

    finish static files before db setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,32 @@
-const colors=require("tailwindcss/colors");
-const defaultTheme=require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ['{pages, app,src}/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/@everybody-gives/ui/**/*.{js,ts}'],
+	content: ['{pages, app,src}/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/@everybody-gives/ui/**/*.{js,ts}'],
 	theme: {
-		colors:{
-		inherit:'inherit',
-		current:'currentColor',
-		transparent:'transparent',
-		black:'#000',
-		white:'#fff',
+		colors: {
+			inherit: 'inherit',
+			current: 'currentColor',
+			transparent: 'transparent',
+			black: '#000',
+			white: '#fff',
 
-		red:colors.red,
-	gray:colors.stone,
-		primary:colors.emerald,
-		background:'#ede7e2',
-		action:'#f5ff7d',
+			red: colors.red,
+			gray: colors.stone,
+			primary: colors.emerald,
+			background: '#ede7e2',
+			action: '#f5ff7d',
 		},
-		fontFamily:{
-			sans:['Arima Marudai', ...defaultTheme.fontFamily.sans],
+		fontFamily: {
+			sans: ['Arima Marudai', ...defaultTheme.fontFamily.sans],
 		},
-		keyframes:{
-			wiggle:{'0%,50%,100%':{transform:'rotate(-3deg) scale(1.2)'},
-			'25%, 75%': {transform: "rotate(3deg) scale(1.2)"},
-			}, 
+		keyframes: {
+			wiggle: {
+				'0%,50%,100%': { transform: 'rotate(-3deg) scale(1.2)' },
+				'25%, 75%': { transform: "rotate(3deg) scale(1.2)" },
+			},
 		},
-		animation:{wiggle:'wiggle 1s ease-in-out infinite'},
+		animation: { wiggle: 'wiggle 1s ease-in-out infinite' },
 		extend: {},
 	},
 	plugins: [require('@tailwindcss/forms')],
