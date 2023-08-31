8036222721a87f4a2c5aaa14692963fe6dd1f312
ckdev88 <ckdev88@outlook.com>
2023-08-31 11:41:54 -0300

    formatting & changed main layout to Layout

--- src/components/GroupInfo.tsx
+++ src/components/GroupInfo.tsx
@@ -1,60 +1,70 @@
-import { useState } from "react"
-import { Button, Card } from "@everybody-gives/ui"
-import { supabase } from "../supabase"
+import { useState } from "react";
+import { Button, Card } from "@everybody-gives/ui";
+import { supabase } from "../supabase";
 
 type GroupInfoProps = {
-  group: {name: string, createdBy: string, id: string}
-  members: {name: string}[]
-  userName: string
-}
+	group: { name: string; createdBy: string; id: string };
+	members: { name: string }[];
+	userName: string;
+};
 
 export const GroupInfo = ({ group, members, userName }: GroupInfoProps) => {
-  const [result, setResult] = useState<string | undefined>(undefined)
+	const [result, setResult] = useState<string | undefined>(undefined);
 
-  const drawPerson = async () => {
-    const { data } = await supabase.rpc("draw_name", { groupid: group.id, username: userName }).single()
-    if (!data) {
-      console.error("No data returned")
-      return
-    }
-    setResult(data)
-  }
-  return (
-    <div>
-      <h1 className="mt-1 text-5xl font-black tracking-tight text-gray-700">
-        Bem vindo em {group.name}, {userName}!
-      </h1>
-      <div className="flex justify-start my-6 items-center">
-        <Button width={215} onClick={() => {
-          void drawPerson()
-        }}>Quem?</Button>
-      </div>
-      <dl>
-        <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-3">
-          <dt className="font-bold text-gray-500">Group url</dt>
-          <dd className="mt-1 text-gray-900 sm:col-span-2 sm:mt-0 flex items-center">
-            http://localhost:3000/{group.id}
-          </dd>
-        </div>
-        <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-3">
-          <dt className="font-bold text-gray-500">Created by</dt>
-          <dd className="mt-1 text-gray-900 sm:col-span-2 sm:mt-0">
-            {group.createdBy}
-          </dd>
-        </div>
-      </dl>
-      <ul className="grid grid-cols-2 gap-6 sm:grid-cols-3 pt-6">
-        {members.map((member) => {
-          let className = " bg-background";
-          if (result === member.name) {
-            className = "bg-action scale-120";
-          }
-          if (result !== member.name && result) {
-            className = " bg-background opacity-50";
-          }
-          return <Card key={member.name} title={member.name} className={className} />;
-        })}
-      </ul>
-    </div>
-  )
-}
+	const drawPerson = async () => {
+		const { data } = await supabase
+			.rpc("draw_name", { groupid: group.id, username: userName })
+			.single();
+		if (!data) {
+			console.error("No data returned");
+			return;
+		}
+		setResult(data);
+	};
+
+	return (
+		<div>
+			<h1 className="mt-1 text-5xl font-black tracking-tight text-gray-700">
+				Bem vindo em {group.name}, {userName}!
+			</h1>
+			<div className="flex justify-start my-6 items-center">
+				<Button
+					width={215}
+					onClick={() => {
+						void drawPerson();
+					}}
+				>
+					Quem?
+				</Button>
+			</div>
+			<dl>
+				<div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-3">
+					<dt className="font-bold text-gray-500">Group url</dt>
+					<dd className="mt-1 text-gray-900 sm:col-span-2 sm:mt-0 flex items-center">
+						http://localhost:3000/{group.id}
+					</dd>
+				</div>
+				<div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-3">
+					<dt className="font-bold text-gray-500">Created by</dt>
+					<dd className="mt-1 text-gray-900 sm:col-span-2 sm:mt-0">
+						{group.createdBy}
+					</dd>
+				</div>
+			</dl>
+			<ul className="grid grid-cols-2 gap-6 sm:grid-cols-3 pt-6">
+				{members.map((member) => {
+					let className = " bg-background";
+					if (result === member.name) {
+						className = "bg-action scale-120";
+					}
+					if (result !== member.name && result) {
+						className = " bg-background opacity-50";
+					}
+					return (
+						<Card key={member.name} title={member.name} className={className} />
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
--- src/components/NewGroupForm.tsx
+++ src/components/NewGroupForm.tsx
@@ -1,69 +1,95 @@
-import { Form, LabeledTextField, Card, InputWithSubmitButton } from '@everybody-gives/ui';
-import { useState } from 'react';
-import { supabase } from '../supabase';
+import {
+	Form,
+	LabeledTextField,
+	Card,
+	InputWithSubmitButton,
+} from "@everybody-gives/ui";
+import { useState } from "react";
+import { supabase } from "../supabase";
 
 export const NewGroupForm = () => {
-	const [groupName, setGroupName] = useState('')
-	const [yourName, setYourName] = useState('')
-	const [members, setMembers] = useState<string[]>([])
+	const [groupName, setGroupName] = useState("");
+	const [yourName, setYourName] = useState("");
+	const [members, setMembers] = useState<string[]>([]);
 
 	return (
-		<Form submitError={null}
-			formTitle='Amigo Secreto'
-			submitText='CREATE'
+		<Form
+			submitError={null}
+			formTitle="Amigo Secreto"
+			submitText="VAMOS"
 			onSubmit={async () => {
 				const { data, error: groupError } = await supabase
-					.from('as_groups')
+					.from("as_groups")
 					.insert({
 						name: groupName,
 						created_by: yourName,
-					}).select('id').single()
+					})
+					.select("id")
+					.single();
 
 				if (groupError) {
-					console.error(groupError)
-					return
+					console.error(groupError);
+					return;
 				}
 
 				if (!data) {
-					console.error('No data returned')
-					return
+					console.error("No data returned");
+					return;
 				}
 				const { error: membersError } = await supabase
-					.from('members')
-					.insert(members.map((name) => ({ name, selected_by: null, group_id: data.id })))
+					.from("as_members")
+					.insert(
+						members.map((name) => ({
+							name,
+							selected_by: null,
+							group_id: data.id,
+						}))
+					);
 
 				if (membersError) {
-					console.log('yey?');
-					console.error(membersError)
-					return
+					console.log("yey?");
+					console.error(membersError);
+					return;
 				}
 
-				window.location.href = `/${data.id}?user=${yourName}`
-			}}>
-
-			<LabeledTextField name="name" label="Group Name" placeholder='my-party-2023' value={groupName} onChange={e => setGroupName(e.target.value)}
+				window.location.href = `/${data.id}?user=${yourName}`;
+			}}
+		>
+			<LabeledTextField
+				name="name"
+				label="Group Name"
+				placeholder="Festa 2023"
+				value={groupName}
+				onChange={(e) => setGroupName(e.target.value)}
 			/>
-			<LabeledTextField name='createdBy' label="your name" placeholder='carla'
+			<LabeledTextField
+				name="createdBy"
+				label="Nome"
+				placeholder="Reginaldo"
 				value={yourName}
-				onChange={e => setYourName(e.target.value)}
+				onChange={(e) => setYourName(e.target.value)}
 			/>
 			<hr />
-			<h3 className='uppercase text-2xl font-bold'>Group members</h3>
+			<h3 className="mt-1 text-xl font-black tracking-tight text-gray-700 text-center">
+				Group members
+			</h3>
 			<InputWithSubmitButton
 				onSubmit={(value) => {
-					setMembers(prev => [...prev, value])
+					setMembers((prev) => [...prev, value]);
 				}}
 			/>
-			<ul className='grid grid-cols-2 gap-6 sm:grid-cols-3'>
+			<ul className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
 				{members.map((memberName, personIdx) => (
 					<Card
-						className='bg-background'
+						className="bg-background"
 						key={personIdx}
 						title={memberName}
-						onDelete={() => { setMembers(prev => prev.filter(m => m !== memberName)) }}
+						onDelete={() => {
+							setMembers((prev) => prev.filter((m) => m !== memberName));
+						}}
 					/>
 				))}
 			</ul>
 		</Form>
-	)
-}
+	);
+};
